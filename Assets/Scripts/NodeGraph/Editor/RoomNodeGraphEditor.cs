2c9ae6d228b145703b3270d8c60bd1fc845349fa
kaedin <kaedinpittman@gmail.com>
2022-11-14 14:39:32 -0500

    almost done with section 5 lesson 10

--- Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -5,6 +5,8 @@ using UnityEditor;
 public class RoomNodeGraphEditor : EditorWindow
 {
     private GUIStyle roomNodeStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+    private RoomNodeTypeListSO roomNodeTypeList;
 
     // Node layout values
     private const float nodeWidth = 160f;
@@ -21,11 +23,23 @@ public class RoomNodeGraphEditor : EditorWindow
         // returns the first editor window 
     }
 
+    // Open the room node graph editor window if a room node graph scriptable object asset is double clicked in the inspector
+    [OnOpeanAsset(0)] // Need the namespace UnityEditor.Callbacks
     public static bool OnDoubleClickAsset(int instanceID, int line)
     {
+        RoomNodeGraphSO roomNodeGraph = EditorUtulity.instanceIDToObject(instanceID) as RoomNodeGraphSO;
 
+        if (roomNodeGraph != null)
+        {
+        
+            OpenWindow();
 
-        private void OnEnable
+            currentRoomNodeGraph = roomNodeGraph;
+
+            return true;
+        }
+        return false;
+        
     }
 
 
@@ -38,18 +52,105 @@ public class RoomNodeGraphEditor : EditorWindow
         roomNodeStyle.normal.textColor = Color.white;
         roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
         roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+
+
+        //Load Room node types
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
     }
 
     private void OnGUI()
     {
-        
-        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-        EditorGUILayout.LabelField("Node 1");
-        GUILayout.EndArea();
+        // if a scriptable object of type RoomNodeGraphSO has been selected then process
+        if (currentRoomNodeGraph != null)
+        {
+            //Process Events
+            ProcessEvents(Event.current);
 
-        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
-        EditorGUILayout.LabelField("Node 2");
-        GUILayout.EndArea();
+            // Draw Room Nodes
+            DrawRoomNodes();
+        }
 
+        if (GUI.changed)
+        Repaint();
+
+    }
+
+    private void ProcessEvents(Event currentEvent)
+    {
+        ProcessRoomNodeGraphEvents(currentEvent);
+    }
+
+    private void ProcessRoomNodeGraphEvents(Event currentEvent)
+    {
+        switch (currentEvent.type)
+        {
+            
+            //Process Mouse Down Events
+            case EventType.MouseDown:
+                ProcessMouseDownEvent(currentEvent);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    private void ProcessMouseDownEvent(Event currentEvent)
+    {
+        // Process right click mouse down on graph event(show context menu)
+        if (currentEvent.button == 1)
+        {
+            ShowContextMenu(currentEvent.mousePosition);
+        }
+    }
+
+    // Show the context menu
+    private void ShowContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu menu = new GenericMenu();
+
+        menu.AddItem(new GUIContent("Create Room Node"), false, CreateRoomNode, mousePosition);
+
+        menu.ShowAsContext();
+    }  
+
+    // Create a room node at the mouse position 
+    private void CreateRoomNode(Object mousePositionObject)
+    {
+        CreateRoomNode(mousePositionObject, roomNodeTypeList.list.Find(x => x.isNone));
+    }  
+
+    //Create a room node at the mouse position = overloaded to also pass in RoomNodeType
+    private void CreateRoomNode(object mousePositionObject, RoomNodeTypeSO roomNodeType)
+    {
+        Vector2 mousePosition = (Vector2)mousePositionObject;
+
+        // create room node scriptable object asset
+        RoomNodeSO roomNode = ScriptableObject.Createinstance<RoomNodeSO>();
+
+        // add room node to current room node graph room node list
+        currentRoomNodeGraph.roomNodeList.Add(roomNode);
+
+        // set room node values
+        roomNode.Initialise(new Rect(mousePosition, new Vector2(modeWidth, nodeHeight)), currentRoomNodeGraph, roomNodeType);
+
+        // ass room node to room node graph scriptable object asset database
+        AssetDatabase.AddObjectToAsset(roomNode, currentRoomNodeGraph);
+
+        AssetDatabase.SaveAssets();
+    }
+
+    /// <sumary>
+    /// Draw room nodes in the graph window
+    /// </sumary>
+    private void DrawRoomNodes()
+    {
+        // loop through all room nodes and draw them
+        foreach (RoomNodeSO roomNode in currentRoomNodeGraph.roomNodeList)
+        {
+            roomNode.Draw(roomNodeStyle);
+        }
+
+        GUI.changed = true;
     }
 }
--- Assets/Scripts/NodeGraph/RoomNodeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class RoomNodeSO : MonoBehaviour
@@ -10,4 +11,34 @@ public class RoomNodeSO : MonoBehaviour
     [HideInInspector] public RoomNodeGraphSO roomNodeGraph;
     public RoomNodeTypeSO roomNodeType;
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
+
+    #region Editor Code
+
+    // the following code should only be run in the Unity Editor
+#if UNITY_EDITOR
+
+    [HideInInspector] public Rect rect;
+
+    // Initalise node
+    public void Initalise(Rect rect, RoomNodeGraphSO nodeGraph, RoomNodeTypeSO roomNodeType)
+    {
+        this.rect = rect;
+        this.id = Guid.NewGuid().ToString();
+        this.name = "RoomNode";
+        this.roomNodeGraph = nodeGraph;
+        this.roomNodeType = roomNodeType;
+
+        // Load room node type list
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
+    }
+
+#endif
+
+    #endregion Editor code
+
+
+
+
+
+
 }
