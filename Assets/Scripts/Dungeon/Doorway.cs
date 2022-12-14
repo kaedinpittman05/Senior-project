c3861904e2c0502aa2fa20d1999bc390f850ee4f
kaedin <kaedinpittman@gmail.com>
2022-12-14 14:52:14 -0500

    started with maps and got map art

---
+++ Assets/Scripts/Dungeon/Doorway.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+[System.Serializable]
+
+public class Doorway : MonoBehaviour
+{
+    public Vector2Int position;
+    public Orientation orientation;
+    public GameObject doorPrefab;
+
+    #region Header
+    [Header("The Upper left Position to Start copying From")]
+    #endregion
+
+    public Vector2Int doorwayStartCopyPosition;
+
+    #region Header
+    [Header("The width of tiles in the doorway to copy over")]
+    #endregion
+
+    public int doorwayCopyTileWidth;
+
+    #region Header
+    [Header("The height of tiles in the doorway to copy over")]
+    #endregion
+
+    public int doorwayCopyTileHeight;
+
+    [HideInInspector]
+    public bool isConnected = false;
+    [HideInInspector]
+    public bool isUnavailable = false;
+}
