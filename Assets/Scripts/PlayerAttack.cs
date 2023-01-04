98090eeb88193c54cc703cc77def719fb3ce6375
Raven Dell <20336@my4county.net>
2023-01-04 14:56:23 -0500

    dell animations and started player attacking

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask enemyLayer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in enemiesHit)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
+}
